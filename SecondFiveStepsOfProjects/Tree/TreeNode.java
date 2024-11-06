6b1d11ce17a03ea9375f525622346fb6ba17e9f2
BASSAM ALHARBI. <101985240+BA-S12@users.noreply.github.com>
2024-11-06 20:04:24 +0300

    Create TreeNode.java

---
+++ SecondFiveStepsOfProjects/Tree/TreeNode.java
@@ -0,0 +1,11 @@
+package SecondFiveStepsOfProjects.Tree;
+
+public class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int value){
+        this.data = value;
+    }
+}
