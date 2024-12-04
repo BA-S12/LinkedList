b72e6bf380b821454e467f9e2217304780c5a377
BASSAM ALHARBI. <101985240+BA-S12@users.noreply.github.com>
2024-12-04 18:00:05 +0300

    Update Garph.java

--- SecondFiveStepsOfProjects/Garph/Garph.java
+++ SecondFiveStepsOfProjects/Garph/Garph.java
@@ -1,5 +1,5 @@
 package SecondFiveStepsOfProjects.Graph;
-
+import FirstFiveStepsOfProject.Queue.Queue;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,7 +7,7 @@ public class Graph {
     private int[][] adjMatrix;
     private Map<String, Integer> vertices;
     private int numOfVertices;
-
+    private int numberOfEdges;
     public Graph(int numberOfVertices){
         this.numOfVertices = numberOfVertices;
         adjMatrix = new int[numberOfVertices][numberOfVertices];
@@ -27,6 +27,7 @@ public class Graph {
             int sourceIndex = vertices.get(source);
             int targetIndex = vertices.get(target);
             adjMatrix[sourceIndex][targetIndex] = 1;
+            numberOfEdges++;
         }
         else {
             System.out.println("One or both vertices is not found");
@@ -74,32 +75,6 @@ public class Graph {
         }
     }
 
-
-
-//    Additional methods
-    public void printAdjMatrix(){
-        System.out.println("Adjacency Matrix: ");
-
-        System.out.print("   ");
-
-        for (String vertex : vertices.keySet()){
-            System.out.print(vertex+" ");
-        }
-        System.out.println();
-
-        for (Map.Entry<String,Integer> rowEntry : vertices.entrySet()){
-            System.out.print(rowEntry.getKey()+ " ");
-            int rowIndex = rowEntry.getValue();
-
-            for (int i = 0; i <numOfVertices ; i++) {
-                System.out.print(adjMatrix[rowIndex][i]+" ");
-            }
-        System.out.println();
-        }
-    }
-
-
-//    Helper Methods
     public void displayOutDegree(){
         System.out.println("out degree: ");
         for (String vertex : vertices.keySet()){
@@ -147,12 +122,162 @@ public class Graph {
             }
         }
     }
+
+    public void displayAllSlingVertices(){
+        System.out.println("AllSlingVertices: ");
+        for(int j=0;j<=adjMatrix.length-1;j++){
+            if(adjMatrix[j][j]==1){
+                System.out.print(this.getVertexByIndex(j)+",");
+            }
+        }
+        System.out.println();
+    }
+
+    public void displayCompleteGraph() {
+        boolean isComplete = true;
+
+        for (int i = 0; i < numOfVertices; i++) {
+            for (int j = 0; j < numOfVertices; j++) {
+                if (i != j && adjMatrix[i][j] == 0) {
+                    isComplete = false;
+                    break;
+                }
+            }
+            if (!isComplete) break;
+        }
+
+        if (isComplete) {
+            System.out.println("The graph is complete.");
+        } else {
+            System.out.println("The graph is not complete.");
+        }
+    }
+
+
+    public void displayConnectedGraph() {
+        boolean[] visited = new boolean[numOfVertices];
+
+        // Start from any vertex (e.g., the first added vertex)
+        dfs(0, visited);
+
+        boolean isConnected = true;
+        for (boolean v : visited) {
+            if (!v) {
+                isConnected = false;
+                break;
+            }
+        }
+
+        if (isConnected) {
+            System.out.println("The graph is connected.");
+        } else {
+            System.out.println("The graph is not connected.");
+        }
+    }
+    public void BFS(String startVertex) {
+        if (!vertices.containsKey(startVertex)) {
+            System.out.println("Start vertex not found.");
+            return;
+        }
+
+        boolean[] visited = new boolean[numOfVertices];
+        Queue queue = new Queue();
+        queue.enqueue(startVertex);
+
+        System.out.println("BFS Traversal:");
+
+        while (!queue.isEmpty()) {
+            String currentVertex = queue.peek();
+            queue.dequeue();
+            int currentIndex = vertices.get(currentVertex);
+
+            if (!visited[currentIndex]) {
+                System.out.print(currentVertex + " ");
+                visited[currentIndex] = true;
+
+                // Add all unvisited adjacent vertices to the queue
+                for (int i = 0; i < numOfVertices; i++) {
+                    if (adjMatrix[currentIndex][i] == 1 && !visited[i]) {
+                        queue.enqueue(getVertexByIndex(i));
+                    }
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public void DFS(String startVertex) {
+        if (!vertices.containsKey(startVertex)) {
+            System.out.println("Start vertex not found.");
+            return;
+        }
+
+        boolean[] visited = new boolean[numOfVertices];
+        System.out.println("DFS Traversal:");
+        dfsHelper(startVertex, visited);
+        System.out.println();
+    }
+
+    public void numberOfEdges(){
+        if(this.numberOfEdges==0)
+            System.out.println("The graph do not have any Edge");
+        else
+            System.out.println("The number of Edges is : "+this.numberOfEdges);
+
+    }
+
+    //    Additional methods
+    public void printAdjMatrix(){
+        System.out.println("Adjacency Matrix: ");
+
+        System.out.print("   ");
+
+        for (String vertex : vertices.keySet()){
+            System.out.print(vertex+" ");
+        }
+        System.out.println();
+
+        for (Map.Entry<String,Integer> rowEntry : vertices.entrySet()){
+            System.out.print(rowEntry.getKey()+ " ");
+            int rowIndex = rowEntry.getValue();
+
+            for (int i = 0; i <numOfVertices ; i++) {
+                System.out.print(adjMatrix[rowIndex][i]+" ");
+            }
+        System.out.println();
+
+        }
+    }
+
+//    Helper Methods
     private String getVertexByIndex(int i){
         for (Map.Entry<String,Integer> entry: vertices.entrySet()){
             if (entry.getValue() == i)
                 return entry.getKey();
         }
         return null;
+    }
+    private void dfs(int index, boolean[] visited) {
+        visited[index] = true;
+        for (int i = 0; i < numOfVertices; i++) {
+            if (adjMatrix[index][i] == 1 && !visited[i]) {
+                dfs(i, visited);
+            }
+        }
+    }
+
+    private void dfsHelper(String currentVertex, boolean[] visited) {
+        int currentIndex = vertices.get(currentVertex);
+        if (visited[currentIndex]) return;
+
+        System.out.print(currentVertex + " ");
+        visited[currentIndex] = true;
+
+        for (int i = 0; i < numOfVertices; i++) {
+            if (adjMatrix[currentIndex][i] == 1 && !visited[i]) {
+                dfsHelper(getVertexByIndex(i), visited);
+            }
+        }
     }
 
 }
